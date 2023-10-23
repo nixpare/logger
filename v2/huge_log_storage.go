f2ad0034dfc347bf526d5351624d46c0a28c712e
Alessio Pareto (Desktop-Pare) <alessio.pareto@nixpare.com>
2023-10-23 19:29:45 +0200

    Huge logger not printing on heavy loads

---
+++ v2/huge_log_storage.go
@@ -0,0 +1,300 @@
+package logger
+
+import (
+	"bufio"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
+)
+
+type hugeLogStorage struct {
+	n         int      // n is the number of logs stored
+	chunks    int      // chunks is the number of files created to store the logs
+	cache     []Log    // cache holds the most recent logs, it is a circular list
+	cacheHead int      // cacheHead points to the start of the cache
+	dir       string   // dir is the directory where the files are saved
+	prefix    string   // prefix holds the identifier of the log files and the timestamp
+	f         *os.File // f is the last log file opened for writing
+	rwm       *sync.RWMutex
+}
+
+func initFileLogStorage(dir, prefix string) (*hugeLogStorage, error) {
+	if !filepath.IsAbs(dir) {
+		wd, _ := os.Getwd()
+		dir = wd + "/" + dir
+	}
+
+	fls := &hugeLogStorage{
+		cache:  make([]Log, 0),
+		dir:    dir,
+		prefix: fmt.Sprintf("%s-%s-", prefix, time.Now().Format(LogFileTimeFormat)),
+		rwm:    new(sync.RWMutex),
+	}
+
+	info, err := os.Stat(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	if !info.IsDir() {
+		return nil, errors.New("the provided path is not a directory")
+	}
+
+	fls.f, err = os.Create(fls.fileNameGeneration(0))
+	if err != nil {
+		return nil, err
+	}
+
+	return fls, nil
+}
+
+func (fls *hugeLogStorage) fileNameGeneration(index int) string {
+	return fmt.Sprintf("%s/%s%d.%s", fls.dir, fls.prefix, index, LogFileExtension)
+}
+
+func (fls *hugeLogStorage) addLog(l Log) int {
+	fls.rwm.Lock()
+	defer fls.rwm.Unlock()
+
+	p := fls.n
+	if len(fls.cache) < LogChunkSize {
+		fls.cache = append(fls.cache, l)
+	} else {
+		fls.cache[fls.cacheHead] = l
+		fls.cacheHead = (fls.cacheHead + 1) % len(fls.cache)
+
+		if fls.n%LogChunkSize == 0 {
+			fls.f.Close()
+
+			fls.chunks++
+			f, err := os.Create(fls.fileNameGeneration(fls.chunks))
+			if err != nil {
+				panic(err)
+			}
+			fls.f = f
+		}
+	}
+	fls.n++
+
+	fls.f.Write(l.JSON())
+	fls.f.Write([]byte{'\n'})
+	return p
+}
+
+func (fls *hugeLogStorage) getLog(index int) Log {
+	fls.rwm.RLock()
+	defer fls.rwm.RUnlock()
+
+	switch {
+	case fls.n <= LogChunkSize:
+		{
+			return fls.cache[index]
+		}
+	case index >= fls.n-LogChunkSize:
+		index = index - (fls.n - LogChunkSize) + fls.cacheHead
+		index %= LogChunkSize
+		return fls.cache[index]
+	}
+
+	fNum := index / LogChunkSize
+	index = index % LogChunkSize
+
+	f, err := os.Open(fls.fileNameGeneration(fNum))
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	sc := bufio.NewScanner(f)
+	for i := 0; i < index; i++ {
+		sc.Scan()
+	}
+	sc.Scan()
+
+	var l Log
+	err = json.Unmarshal(sc.Bytes(), &l)
+	if err != nil {
+		panic(err)
+	}
+
+	return l
+}
+
+type interval struct {
+	start, end int
+}
+
+func (fls hugeLogStorage) splitRequestRange(start, end int) (res []interval) {
+	if end-1 >= fls.n-LogChunkSize {
+		if start < fls.n-LogChunkSize {
+			defer func(end int) {
+				res = append(res, interval{
+					start: fls.n - LogChunkSize,
+					end:   end,
+				})
+			}(end)
+
+			end = fls.n - LogChunkSize
+		} else {
+			res = append(res, interval{
+				start: start,
+				end:   end,
+			})
+			return
+		}
+	}
+
+	inter := interval{start: start, end: start + 1}
+
+	for i := start + 1; i < end; i++ {
+		if i%LogChunkSize == 0 {
+			res = append(res, inter)
+			inter = interval{start: i, end: i + 1}
+		} else {
+			inter.end++
+		}
+	}
+	res = append(res, inter)
+
+	return
+}
+
+func (fls *hugeLogStorage) getLogs(start, end int) []Log {
+	fls.rwm.RLock()
+	defer fls.rwm.RUnlock()
+
+	inter := fls.splitRequestRange(start, end)
+	res := make([]Log, 0, end-start)
+
+	for _, x := range inter {
+		if x.start >= fls.n-LogChunkSize {
+			for i := x.start; i < x.end; i++ {
+				res = append(res, fls.getLog(i))
+			}
+		} else {
+			fNum := x.start / LogChunkSize
+
+			f, err := os.Open(fls.fileNameGeneration(fNum))
+			if err != nil {
+				panic(err)
+			}
+			defer f.Close()
+
+			sc := bufio.NewScanner(f)
+			for i := fNum * LogChunkSize; i < x.start; i++ {
+				sc.Scan()
+			}
+
+			for i := x.start; i < x.end; i++ {
+				sc.Scan()
+
+				var l Log
+				err = json.Unmarshal(sc.Bytes(), &l)
+				if err != nil {
+					panic(err)
+				}
+
+				res = append(res, l)
+			}
+		}
+	}
+
+	return res
+}
+
+func (fls hugeLogStorage) splitRequestSingle(logs []int) (res [][]int) {
+	if len(logs) == 0 {
+		return
+	}
+
+	if logs[len(logs)-1] >= fls.n-LogChunkSize {
+		var inter []int
+		var i int
+
+		func() {
+			for i = len(logs) - 2; i >= 0 && logs[i] >= fls.n-LogChunkSize; i-- {
+				defer func(p int) {
+					inter = append(inter, p)
+				}(logs[i])
+			}
+		}()
+		inter = append(inter, logs[len(logs)-1])
+
+		defer func(inter []int) {
+			res = append(res, inter)
+		}(inter)
+		logs = logs[:i+1]
+	}
+
+	if len(logs) == 0 {
+		return
+	}
+
+	inter := []int{logs[0]}
+	for i := 1; i < len(logs); i++ {
+		if logs[i]/LogChunkSize == inter[0]/LogChunkSize {
+			inter = append(inter, logs[i])
+			continue
+		}
+
+		res = append(res, inter)
+		inter = []int{logs[i]}
+	}
+	res = append(res, inter)
+
+	return
+}
+
+func (fls *hugeLogStorage) getSpecificLogs(logs []int) []Log {
+	fls.rwm.RLock()
+	defer fls.rwm.RUnlock()
+
+	inter := fls.splitRequestSingle(logs)
+	res := make([]Log, 0, len(logs))
+
+	for _, i := range inter {
+		if i[0] >= fls.n-LogChunkSize {
+			for _, p := range i {
+				res = append(res, fls.getLog(p))
+			}
+		} else {
+			fNum := i[0] / LogChunkSize
+
+			f, err := os.Open(fls.fileNameGeneration(fNum))
+			if err != nil {
+				panic(err)
+			}
+			defer f.Close()
+
+			sc := bufio.NewScanner(f)
+			lastRead := (fNum * LogChunkSize) - 1
+
+			for _, p := range i {
+				for j := lastRead + 1; j < p; j++ {
+					sc.Scan()
+				}
+
+				sc.Scan()
+				lastRead = p
+
+				var l Log
+				err = json.Unmarshal(sc.Bytes(), &l)
+				if err != nil {
+					panic(err)
+				}
+
+				res = append(res, l)
+			}
+		}
+	}
+
+	return res
+}
+
+func (fls *hugeLogStorage) nLogs() int {
+	return fls.n
+}
--- v2/huge_logger.go
+++ v2/huge_logger.go
@@ -1,22 +1,24 @@
 package logger
 
 import (
-	"bufio"
-	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
-	"os"
-	"path/filepath"
 	"sync"
 	"time"
+
+	"github.com/nixpare/comms"
 )
 
 type hugeLogger struct {
 	out             io.Writer
-	storage         *fileLogStorage
+	fls             *hugeLogStorage
 	tags            []string
 	extrasDisabled  bool
+	counter         int
+	heavyLoad       bool
+	lastWrote       int
+	writingM        *sync.Mutex
+	stopBc          *comms.Broadcaster[struct{}]
 }
 
 // NewLogger creates a logger that keeps in memory the most recent logs and
@@ -29,26 +31,40 @@ func NewHugeLogger(out io.Writer, dir string, prefix string, tags ...string) (Lo
 		return nil, err
 	}
 
-	return &hugeLogger{
-		out:     out,
-		storage: fls,
-		tags:    tags,
-	}, nil
+	l := &hugeLogger{
+		out:      out,
+		fls:      fls,
+		tags:     tags,
+		writingM: new(sync.Mutex),
+		stopBc:   comms.NewBroadcaster[struct{}](),
+	}
+
+	go l.checkHeavyLoad()
+	return l, nil
 }
 
 func (l *hugeLogger) newLog(log Log, writeOutput bool) int {
 	log.addTags(l.tags...)
-	p := l.storage.addLog(log)
+	p := l.fls.addLog(log)
 
 	if l.out == nil || !writeOutput {
 		return p
 	}
 
-	logToOut(l, log, l.extrasDisabled)
+	l.writingM.Lock()
+	defer l.writingM.Unlock()
+
+	if !l.heavyLoad && l.lastWrote == p-1 {
+		l.lastWrote = p
+		logToOut(l, log, l.extrasDisabled)
+	}
+
 	return p
 }
 
 func (l *hugeLogger) AddLog(level LogLevel, message string, extra string, writeOutput bool) {
+	l.counter++
+
 	l.newLog(Log{
 		l: newLog(level, message, extra),
 	}, writeOutput)
@@ -67,7 +83,7 @@ func (l *hugeLogger) Debug(a ...any) {
 }
 
 func (l *hugeLogger) NLogs() int {
-	return l.storage.nLogs()
+	return l.fls.nLogs()
 }
 
 func (l *hugeLogger) Out() io.Writer {
@@ -75,11 +91,11 @@ func (l *hugeLogger) Out() io.Writer {
 }
 
 func (l *hugeLogger) GetLog(index int) Log {
-	return l.storage.getLog(index)
+	return l.fls.getLog(index)
 }
 
 func (l *hugeLogger) GetLastNLogs(n int) []Log {
-	tot := l.storage.nLogs()
+	tot := l.fls.nLogs()
 	if n > tot {
 		n = tot
 	}
@@ -87,11 +103,11 @@ func (l *hugeLogger) GetLastNLogs(n int) []Log {
 }
 
 func (l *hugeLogger) GetLogs(start, end int) []Log {
-	return l.storage.getLogs(start, end)
+	return l.fls.getLogs(start, end)
 }
 
 func (l *hugeLogger) GetSpecificLogs(logs []int) []Log {
-	return l.storage.getSpecificLogs(logs)
+	return l.fls.getSpecificLogs(logs)
 }
 
 func (l *hugeLogger) Write(p []byte) (n int, err error) {
@@ -110,294 +126,69 @@ func (l *hugeLogger) Clone(out io.Writer, parentOut bool, tags ...string) Logger
 	return newCloneLogger(l, out, tags, l.extrasDisabled, parentOut)
 }
 
-func (l *hugeLogger) Close() {
-	panic("To be implemented")
-}
-
-type fileLogStorage struct {
-	n int 				// n is the number of logs stored
-	chunks int 			// chunks is the number of files created to store the logs
-	cache []Log 		// cache holds the most recent logs, it is a circular list
-	cacheHead int 		// cacheHead points to the start of the cache
-	dir string 			// dir is the directory where the files are saved
-	prefix string 		// prefix holds the identifier of the log files and the timestamp
-	f *os.File 			// f is the last log file opened for writing
-	rwm *sync.RWMutex
-}
-
-func initFileLogStorage(dir, prefix string) (*fileLogStorage, error) {
-	if !filepath.IsAbs(dir) {
-		wd, _ := os.Getwd()
-		dir = wd + "/" + dir
-	}
+func (l *hugeLogger) checkHeavyLoad() {
+	ticker := time.NewTicker(time.Second)
+	var exitLoop bool
 	
-	fls := &fileLogStorage{
-		cache: make([]Log, 0),
-		dir: dir,
-		prefix: fmt.Sprintf("%s-%s-", prefix, time.Now().Format(LogFileTimeFormat)),
-		rwm: new(sync.RWMutex),
-	}
+	stopC := make(chan struct{})
+	defer close(stopC)
 
-	info, err := os.Stat(dir)
-	if err != nil {
-		return nil, err
-	}
-
-	if !info.IsDir() {
-		return nil, errors.New("the provided path is not a directory")
-	}
-
-	fls.f, err = os.Create(fls.fileNameGeneration(0))
-	if err != nil {
-		return nil, err
-	}
-
-	return fls, nil
-}
-
-func (fls *fileLogStorage) fileNameGeneration(index int) string {
-	return fmt.Sprintf("%s/%s%d.%s", fls.dir, fls.prefix, index, LogFileExtension)
-}
-
-func (fls *fileLogStorage) addLog(l Log) int {
-	fls.rwm.Lock()
-	defer fls.rwm.Unlock()
-
-	p := fls.n
-	if len(fls.cache) < LogChunkSize {
-		fls.cache = append(fls.cache, l)
-	} else {
-		fls.cache[fls.cacheHead] = l
-		fls.cacheHead = (fls.cacheHead + 1) % len(fls.cache)
-
-		if fls.n % LogChunkSize == 0 {
-			fls.f.Close()
-
-			fls.chunks ++
-			f, err := os.Create(fls.fileNameGeneration(fls.chunks))
-			if err != nil {
-				panic(err)
-			}
-			fls.f = f
-		}
-	}
-	fls.n ++
-
-	fls.f.Write(l.JSON())
-	fls.f.Write([]byte{'\n'})
-	return p
-}
-
-func (fls *fileLogStorage) getLog(index int) Log {
-	fls.rwm.RLock()
-	defer fls.rwm.RUnlock()
-
-	switch {
-	case fls.n <= LogChunkSize: {
-		return fls.cache[index]
-	}
-	case index >= fls.n - LogChunkSize:
-		index = index - (fls.n - LogChunkSize) + fls.cacheHead
-		index %= LogChunkSize
-		return fls.cache[index]
-	}
-
-	fNum := index / LogChunkSize
-	index = index % LogChunkSize
-
-	f, err := os.Open(fls.fileNameGeneration(fNum))
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	sc := bufio.NewScanner(f)
-	for i := 0; i < index; i++ {
-		sc.Scan()
-	}
-	sc.Scan()
+	var stopMsg comms.BroadcastMessage[struct{}]
+	go func() {
+		stopMsg = l.stopBc.Listen()
+		stopC <- struct{}{}
+	}()
 	
-	var l Log
-	err = json.Unmarshal(sc.Bytes(), &l)
-	if err != nil {
-		panic(err)
-	}
+	var alignInProgress bool
 
-	return l
-}
-
-type interval struct {
-	start, end int
-}
-
-func (fls fileLogStorage) splitRequestRange(start, end int) (res []interval) {
-	if end-1 >= fls.n - LogChunkSize {
-		if start < fls.n - LogChunkSize {
-			defer func(end int) {
-				res = append(res, interval{
-					start: fls.n - LogChunkSize,
-					end: end,
-				})
-			}(end)
-			
-			end = fls.n - LogChunkSize
-		} else {
-			res = append(res, interval{
-				start: start,
-				end: end,
-			})
-			return
-		}
-	}
-
-	inter := interval{ start: start, end: start+1 }
-	
-	for i := start+1; i < end; i++ {
-		if i % LogChunkSize == 0 {
-			res = append(res, inter)
-			inter = interval{ start: i, end: i+1 }
-		} else {
-			inter.end ++
-		}
-	}
-	res = append(res, inter)
-
-	return
-}
-
-func (fls*fileLogStorage) getLogs(start, end int) []Log {
-	fls.rwm.RLock()
-	defer fls.rwm.RUnlock()
-
-	inter := fls.splitRequestRange(start, end)
-	res := make([]Log, 0, end-start)
-
-	for _, x := range inter {
-		if x.start >= fls.n - LogChunkSize {
-			for i := x.start; i < x.end; i++ {
-				res = append(res, fls.getLog(i))
-			}
-		} else {
-			fNum := x.start / LogChunkSize
-
-			f, err := os.Open(fls.fileNameGeneration(fNum))
-			if err != nil {
-				panic(err)
-			}
-			defer f.Close()
-
-			sc := bufio.NewScanner(f)
-			for i := fNum * LogChunkSize; i < x.start; i++ {
-				sc.Scan()
-			}
-
-			for i := x.start; i < x.end; i++ {
-				sc.Scan()
+	for !exitLoop {
+		select {
+		case <- ticker.C:
+			if l.counter > MaxLogsPerSec {
+				l.heavyLoad = true
+			} else {
+				if !alignInProgress {
+					go func() {
+						alignInProgress = true
+						l.alignOutput()
+						alignInProgress = false
+					}()
+				}
 				
-				var l Log
-				err = json.Unmarshal(sc.Bytes(), &l)
-				if err != nil {
-					panic(err)
-				}
-
-				res = append(res, l)
+				l.heavyLoad = false
 			}
+			l.counter = 0
+		case <- stopC:
+			ticker.Stop()
+			l.alignOutput()
+			exitLoop = true
 		}
 	}
 
-	return res
+	stopMsg.Report()
 }
 
-func (fls fileLogStorage) splitRequestSingle(logs []int) (res [][]int) {
-	if len(logs) == 0 {
+func (l *hugeLogger) Close() {
+	l.stopBc.SendAndWait(struct{}{})
+}
+
+func (l *hugeLogger) alignOutput() {
+	if l.fls.n == 0 {
 		return
 	}
 
-	if logs[len(logs)-1] >= fls.n - LogChunkSize {
-		var inter []int
-		var i int
+	l.writingM.Lock()
+	defer l.writingM.Unlock()
 
-		func() {
-			for i = len(logs)-2; i >= 0 && logs[i] >= fls.n - LogChunkSize; i-- {
-				defer func(p int) {
-					inter = append(inter, p)
-				}(logs[i])
-			}
-		}()
-		inter = append(inter, logs[len(logs)-1])
-
-		defer func(inter []int) {
-			res = append(res, inter)
-		}(inter)
-		logs = logs[:i+1]
-	}
-
-	if len(logs) == 0 {
-		return
-	}
-
-	inter := []int{logs[0]}
-	for i := 1; i < len(logs); i++ {
-		if logs[i] / LogChunkSize == inter[0] / LogChunkSize {
-			inter = append(inter, logs[i])
-			continue
+	for {
+		logs := l.fls.getLogs(l.lastWrote+1, l.fls.n)
+		if len(logs) == 0 {
+			break
 		}
 
-		res = append(res, inter)
-		inter = []int{logs[i]}
-	}
-	res = append(res, inter)
-
-	return
-}
-
-func (fls*fileLogStorage) getSpecificLogs(logs []int) []Log {
-	fls.rwm.RLock()
-	defer fls.rwm.RUnlock()
-
-	inter := fls.splitRequestSingle(logs)
-	res := make([]Log, 0, len(logs))
-
-	for _, i := range inter {
-		if i[0] >= fls.n - LogChunkSize {
-			for _, p := range i {
-				res = append(res, fls.getLog(p))
-			}
-		} else {
-			fNum := i[0] / LogChunkSize
-
-			f, err := os.Open(fls.fileNameGeneration(fNum))
-			if err != nil {
-				panic(err)
-			}
-			defer f.Close()
-
-			sc := bufio.NewScanner(f)
-			lastRead := (fNum * LogChunkSize) - 1
-
-			for _, p := range i {
-				for j := lastRead + 1; j < p; j++ {
-					sc.Scan()
-				}
-
-				sc.Scan()
-				lastRead = p
-
-				var l Log
-				err = json.Unmarshal(sc.Bytes(), &l)
-				if err != nil {
-					panic(err)
-				}
-
-				res = append(res, l)
-			}
+		for _, log := range logs {
+			logToOut(l, log, l.extrasDisabled)
 		}
+		l.lastWrote += len(logs)
 	}
-
-	return res
 }
-
-func (fls *fileLogStorage) nLogs() int {
-	return fls.n
-}
-
